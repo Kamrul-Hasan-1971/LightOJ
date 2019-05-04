2c205e743308c9a26e77e35eb5a2f9d932a66cca
Kamrul_Hasan <48267671+kicchuparinah@users.noreply.github.com>
2019-05-04 13:01:21 +0600

    Add files via upload

---
+++ 1241 - Pinocchio.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+///...................................*****.................................................///
+///                  Kamrul Hasan ( icthasan36@gmail.com )                                  ///
+///                  Department of Ict                                                      ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
+
+
+
+#define           CIN              ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define           ll               long long int
+#define           ull              unsigned long long int
+#define           pii              pair < int, int>
+#define           pll              pair < ll, ll>
+#define           MOD              1000000007
+#define           vi               vector<int>
+#define           vl               vector<ll>
+#define           pb               push_back
+#define           sc               scanf
+#define           pf               printf
+#define         scin(x)            scanf("%d",&(x))
+#define        scin2(x,y)          scanf("%d %d",&(x),&(y))
+#define         scln(x)            scanf("%lld",&(x))
+#define        scln2(x,y)          scanf("%lld %lld",&(x),&(y))
+#define       min3(a,b,c)          min(a,min(b,c))
+#define       min4(a,b,c,d)        min(d,min(a,min(b,c)))
+#define       max3(a,b,c)          max(a,max(b,c))
+#define       max4(a,b,c,d)        max(d,max(a,max(b,c)))
+#define         ms(a,b)            memset(a,b,sizeof(a))
+#define           mp               make_pair
+#define       gcd(a, b)            __gcd(a,b)
+#define       lcm(a, b)            ((a)*(b)/gcd(a,b))
+#define         input              freopen("input.txt","rt", stdin)
+#define         output             freopen("output.txt","wt", stdout)
+#define           PI               2*acos(0.0)
+#define     rep( i , a , b )       for( i=a ; i<b ; i++)
+#define     rev( i , a , b )       for( i=a ; i>=b ; i--)
+#define     repx( i ,a,b, x)       for( i=a ; i<b ; i+=x)
+#define       CASEL(t)             printf("Case %d:\n",t)
+#define     RUN_CASE(t,T)          for(__typeof(t) t=1;t<=T;t++)
+#define zero(a) memset(a, 0 , sizeof(a))
+
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+int main()
+{
+    vector<ll>v;
+    ll a,b,c,i,j,cnt=0,g,h,u,t;
+    cin>>a;
+    for(i=0; i<a; i++)
+    {
+        cin>>b;
+        for(j=0; j<b; j++)
+        {
+            cin>>c;
+            v.pb(c);
+        }
+        g=v[0];
+        if(g!=2)
+        {
+            h=g-2;
+            u=h/5;
+            t=u+1;
+            if(h%5==0)
+            {
+                t=t-1;
+            }
+            cnt=cnt+t;
+        }
+        for(j=1; j<b; j++)
+        {
+            if(v[j]==2)
+            {
+                g=2;
+            }
+            else if(v[j]>g)
+            {
+                h=v[j]-g;
+                u=h/5;
+                t=u+1;
+                if(h%5==0)
+                {
+                    t=t-1;
+                }
+                cnt=cnt+t;
+                g=v[j];
+            }
+        }
+        printf("Case %lld: %lld\n",i+1,cnt);
+        cnt=0;
+        v.clear();
+    }
+    return 0;
+}
