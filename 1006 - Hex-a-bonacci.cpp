531de7ef5df44652c2f378f94c3a032a27cabbf4
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2019-09-26 07:18:30 +0600

    Update 1006 - Hex-a-bonacci.cpp

--- 1006 - Hex-a-bonacci.cpp
+++ 1006 - Hex-a-bonacci.cpp
@@ -1,81 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-///...................................*****.................................................///
-///                  Kamrul Hasan ( icthasan36@gmail.com )                                  ///
-///                  Department of Ict                                                      ///
-///                  Comilla University , Bangladesh.                                       ///
-///...................................*****.................................................///
-
-
-
-#define           CIN              ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define           ll               long long int
-#define           ull              unsigned long long int
-#define           pii              pair < int, int>
-#define           pll              pair < ll, ll>
-#define           MOD              1000000007
-#define           vi               vector<int>
-#define           vl               vector<ll>
-#define           pb               push_back
-#define           sc               scanf
-#define           pf               printf
-#define         scin(x)            scanf("%d",&(x))
-#define        scin2(x,y)          scanf("%d %d",&(x),&(y))
-#define         scln(x)            scanf("%lld",&(x))
-#define        scln2(x,y)          scanf("%lld %lld",&(x),&(y))
-#define       min3(a,b,c)          min(a,min(b,c))
-#define       min4(a,b,c,d)        min(d,min(a,min(b,c)))
-#define       max3(a,b,c)          max(a,max(b,c))
-#define       max4(a,b,c,d)        max(d,max(a,max(b,c)))
-#define         ms(a,b)            memset(a,b,sizeof(a))
-#define           mp               make_pair
-#define       gcd(a, b)            __gcd(a,b)
-#define       lcm(a, b)            ((a)*(b)/gcd(a,b))
-#define         input              freopen("input.txt","rt", stdin)
-#define         output             freopen("output.txt","wt", stdout)
-#define           PI               2*acos(0.0)
-#define     rep( i , a , b )       for( i=a ; i<b ; i++)
-#define     rev( i , a , b )       for( i=a ; i>=b ; i--)
-#define     repx( i ,a,b, x)       for( i=a ; i<b ; i+=x)
-#define       CASEL(t)             printf("Case %d:\n",t)
-#define     RUN_CASE(t,T)          for(__typeof(t) t=1;t<=T;t++)
-#define zero(a) memset(a, 0 , sizeof(a))
+#define           mod              10000007
+#define           zero(a)          memset(a,-1,sizeof a)
 
-///--------------Graph Moves--------------------------------------
-///const int fx[] = {+1,-1,+0,+0};
-///const int fy[] = {+0,+0,+1,-1};
-///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
-///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
-///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
-///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
-///---------------------------------------------------------------
+ll  a, b, c, d, e, f  ;
 
-ull ara[10005];
-ull a, b, c, d, e, f;
-ull fn( ull n ) {
-    if( n == 0 ) return a% 10000007;
-    else if( n == 1 ) return b% 10000007;
-    else if( n == 2 ) return c% 10000007;
-    else if( n == 3 ) return d% 10000007;
-    else if( n == 4 ) return e% 10000007;
-    else if( n == 5 ) return f% 10000007;
-    else if(ara[n]==0){
+ll dp[10005];
 
-    ara[n]=fn(n-1) + fn(n-2) + fn(n-3) + fn(n-4) + fn(n-5) + fn(n-6) ;
-    ara[n]=ara[n]% 10000007;
-    }
-    return ara[n];
+ll fun(ll n)
+{
+    if( n == 0 )
+        return a%mod;
+    if( n == 1 )
+        return b%mod;
+    if( n == 2 )
+        return c%mod;
+    if( n == 3 )
+        return d%mod;
+    if( n == 4 )
+        return e%mod;
+    if( n == 5 )
+        return f%mod;
+    if(dp[n]!=-1)
+        return dp[n] ;
+    dp[n] = ( fun(n-1) % mod + fun(n-2) % mod + fun(n-3) % mod + fun(n-4) % mod + fun(n-5) % mod + fun(n-6) %mod ) % mod ;
+    return dp[n] % mod ;
 }
-int main() {
-    ull n, caseno = 0, cases,i;
-    scanf("%llu", &cases);
-    while( cases-- ) {
-        scanf("%llu %llu %llu %llu %llu %llu %llu", &a, &b, &c, &d, &e, &f, &n);
-        printf("Case %llu: %llu\n", ++caseno, fn(n));
-        for(i=0;i<10002;i++){
-            ara[i]=0;
-        }
+
+int main()
+{
+    ll test, n,i ;
+    scanf("%lld",&test);
+    for( i  = 1 ; i <= test ; i++ )
+    {
+        cin>>a>>b>>c>>d>>e>>f>>n;
+        zero(dp);
+        printf("Case %lld: %lld\n",i,fun(n));
     }
     return 0;
 }
+
